a0519ab087e8a8599ee0f87ce32abc1a59a0a1e5
Alcartez <67427418+Alcartez@users.noreply.github.com>
2021-03-22 12:03:38 +0530

    Update install_ngs_te_mapper.py

--- installation/install_ngs_te_mapper.py
+++ installation/install_ngs_te_mapper.py
@@ -5,22 +5,22 @@ import subprocess
 
 def execute(url, commit, log, install_path):
     if os.path.exists(install_path):
-        shutil.rmtree(install_path)
+        shutil.rmtree(install_path) #If install path exists then remove the directory entirely 
 
     try:
-        os.makedirs(install_path)
+        os.makedirs(install_path) # Make directory for installation
     except OSError:
-        pass
+        pass #If there's an OS error , output nothing. 
 
     with open(log, "w") as f:
-        subprocess.check_call([f"git clone {url} {install_path}"], shell=True, stderr=f, stdout=f)
+        subprocess.check_call([f"git clone {url} {install_path}"], shell=True, stderr=f, stdout=f) #Clone the repository
 
     with open(log, "a+") as f:
-        subprocess.check_call(['git checkout ' + commit], stdout=f, stderr=f, shell=True, cwd=install_path)
-
+        subprocess.check_call(['git checkout ' + commit], stdout=f, stderr=f, shell=True, cwd=install_path) # git checkout the repository
 
 if __name__ == "__main__":
     # noinspection PyUnresolvedReferences
+    #Execute Snakemake for install
     execute(url=snakemake.params.url,
             commit=snakemake.params.commit,
             log=snakemake.params.log,
